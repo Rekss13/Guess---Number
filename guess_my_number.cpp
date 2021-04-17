266e897d1d38fa78464c30fd09ce007bdf09267a
Rekss <rekss13@gmail.com>
2021-04-17 19:19:39 +0300

    added game code

--- guess_my_number.cpp
+++ guess_my_number.cpp
@@ -1,5 +1,27 @@
 ﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
 int main(){
+    cout << "\tWelcom to Guess My Number\n\n";
     
+    srand(static_cast<unsigned int>(time(0)));
+    int secretNumber = rand() % 100 + 1;
+    int tries = 0;
+    int guess;
+
+    do{
+        cout << "Enter a guess: ";
+        cin >> guess;
+        if (guess > secretNumber)
+            cout << "Too high!\n\n";
+        else if (guess < secretNumber)
+            cout << "Too low!\n\n";
+        ++tries;
+    } while (guess != secretNumber);
+
+    cout << "\nThat's it! You got it in " << tries << " guesses!\n";
+
+    return 0;
 }
